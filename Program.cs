c8e07b57a75547d8a34997cac2e62896ca4f869e
walter hu <walter.git@outlook.com>
2023-02-22 09:18:34 +0800

    test

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace core3Demo
         }
        public static RequestDelegate FooMiddleware(RequestDelegate next) => async context =>
         {
-            //6666
+            //888
             await context.Response.WriteAsync("Foo=>");
             await next(context);
         };
